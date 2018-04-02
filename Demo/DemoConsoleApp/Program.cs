3d2182e5fcdb5be1d42322f9177785289cd557fe
Oldrich Kozelsky <oldrich.kozelsky@email.cz>
2018-04-02 16:17:50 +0200

    Exception handling

--- Demo/DemoConsoleApp/Program.cs
+++ Demo/DemoConsoleApp/Program.cs
@@ -8,14 +8,29 @@ namespace RCNet.DemoConsoleApp
     {
         static void Main(string[] args)
         {
-            //Logging the output to a console
-            IOutputLog demoOutputLog = new ConsoleLog();
-            //Esn demo
-            string esnDemoSettingsFile = @"EsnDemoSettings.xml";
-            EsnDemo.RunDemo(demoOutputLog, esnDemoSettingsFile);
+            try
+            {
+                //Logging the output to a console
+                IOutputLog demoOutputLog = new ConsoleLog();
+                //Esn demo
+                string esnDemoSettingsFile = @"EsnDemoSettings.xml";
+                EsnDemo.RunDemo(demoOutputLog, esnDemoSettingsFile);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine();
+                while(e != null)
+                {
+                    Console.WriteLine(e.Message);
+                    e = e.InnerException;
+                }
+                Console.WriteLine("Press Enter to continue");
+            }
             Console.ReadLine();
             return;
         }
+
+
     }//Program
 
 }//Namespace
